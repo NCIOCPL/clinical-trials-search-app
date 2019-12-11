34e0c8e7adacc65ba14edac1591b4efdaa574f09
Bryan Pizzillo <bryanpizzillo@users.noreply.github.com>
2019-12-11 13:35:05 -0500

    Added params to the results_page_link event data - pageNum - formType

--- src/views/ResultsPage/ResultsList.jsx
+++ src/views/ResultsPage/ResultsList.jsx
@@ -5,7 +5,10 @@ import ResultsListItem from './ResultsListItem';
 
 
 const ResultsList = ({ results, selectedResults, setSelectedResults, setSelectAll, queryParams, tracking }) => {
-  const resultsPage = useSelector(store => store.form.resultsPage);
+  const { 
+    resultsPage,
+    formType 
+  } = useSelector(store => store.form);
 
   const handleOnCheckChange = id => {
     let resultItem = {
@@ -33,6 +36,8 @@ const ResultsList = ({ results, selectedResults, setSelectedResults, setSelectAl
             id={item.nciID}
             item={item}
             itemIndex={idx}
+            resultsPage={resultsPage}
+            formType={formType}
             isChecked={selectedResults.find( ({id}) => id === item.nciID) !== undefined}
             onCheckChange={handleOnCheckChange}
             tracking={tracking}
