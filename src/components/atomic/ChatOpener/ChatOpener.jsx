6564fb7386de2d09cd3d2600cfadcbf4954645f1
Seyi Longe <oluwaseun.longe@nih.gov>
2020-12-14 16:52:03 -0500

    (#203) No Trials Search Page Live Help Link
    Closes #203
    
    * Using form action to open "chat online" live help
    * Updated style and swapped "p" for "div" element
    * Updated style for chat link

--- src/components/atomic/ChatOpener/ChatOpener.jsx
+++ src/components/atomic/ChatOpener/ChatOpener.jsx
@@ -1,22 +1,23 @@
 import React from 'react';
 
 const ChatOpener = () => {
-  const handleChatSubmit = () => {
-    window.open(
-      'https://livehelp.cancer.gov/app/chat/chat_landing?_icf_22=2174',
-      'Live_Assistance',
-      'status=1,toolbar=0,menubar=0,location=0,resizable=1,height=750,width=660left=100,top=100'
-    );
-  };
 
   return (
-    <button
-      className="btnAsLink chat-link"
-      type="button"
-      onClick={handleChatSubmit}
+    <form action="https://livehelp.cancer.gov/app/chat/chat_landing"
+          method="post"
+          target="Live_Assistance"
+          className="inline-form"
+          onSubmit={ () => {
+            return window.open(
+              'about:blank',
+              'Live_Assistance',
+              'status=1,toolbar=0,menubar=0,location=0,resizable=1,height=750,width=660left=100,top=100'
+            );
+          }}
     >
-      chat online
-    </button>
+      <input name="_icf_22" type="hidden" value="2174" />
+      <button className="btnAsLink chat-link" type="submit">chat online</button>
+    </form>
   );
 };
 
