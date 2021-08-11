9967f68f7135e7336510b16e99e9b956fe5193ba
Bryan Pizzillo <bryanpizzillo@users.noreply.github.com>
2021-08-11 18:12:17 -0400

    (#261) Fix issue with unit tests failing

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,5 +1,8 @@
 import { configure } from 'enzyme';
+import axios from 'axios';
 import Adapter from 'enzyme-adapter-react-16';
 
 configure({ adapter: new Adapter() });
 
+// Resolves issues with 'Error: Cross origin http://localhost forbidden'
+axios.defaults.adapter = require('axios/lib/adapters/http');
