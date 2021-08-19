1890d98635a2877ce7a93f055b42691b53c46637
Bryan Pizzillo <bryanpizzillo@users.noreply.github.com>
2021-08-19 17:02:54 -0400

    (#276) Fix Cypress Linting Errors

--- cypress/integration/common/beforeEach.js
+++ cypress/integration/common/beforeEach.js
@@ -7,22 +7,22 @@ beforeEach(() => {
 		// page. Setting all defaults in order to make sure that a change
 		// to development defaults does not break a bunch of texts.
 		win.INT_TEST_APP_PARAMS = {
-      analyticsName: 'Clinical Trials',
-      appId: '@@/DEFAULT_CTS_APP_ID',
-      baseHost: 'http://localhost:3000',
-      basePath: '/about-cancer/treatment/clinical-trials/search',
-      canonicalHost: 'https://www.cancer.gov',
-      ctsTitle: 'Find NCI-Supported Clinical Trials',
-      language: 'en',
-      rootId: 'NCI-CTS-root',
-      siteName: 'National Cancer Institute',
-      title: 'NCI Drug Dictionary',
-      contentGroup: 'Clinical Trials',
-      zipConversionEndpoint: '/mock-api/zip_code_lookup',
-      ctsHostname: 'localhost',
-      ctsProtocol: 'http',
-      ctsPort: 3000
-    };
+			analyticsName: 'Clinical Trials',
+			appId: '@@/DEFAULT_CTS_APP_ID',
+			baseHost: 'http://localhost:3000',
+			basePath: '/about-cancer/treatment/clinical-trials/search',
+			canonicalHost: 'https://www.cancer.gov',
+			ctsTitle: 'Find NCI-Supported Clinical Trials',
+			language: 'en',
+			rootId: 'NCI-CTS-root',
+			siteName: 'National Cancer Institute',
+			title: 'NCI Drug Dictionary',
+			contentGroup: 'Clinical Trials',
+			zipConversionEndpoint: '/mock-api/zip_code_lookup',
+			ctsHostname: 'localhost',
+			ctsProtocol: 'http',
+			ctsPort: 3000,
+		};
 		console.log(win.INT_TEST_APP_PARAMS);
 	});
 });
