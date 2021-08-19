ae8cb8ea81bdf3439fd63a8d83bc8afd7cd67355
Bryan Pizzillo <bryanpizzillo@users.noreply.github.com>
2021-08-19 17:04:25 -0400

    (#276) Enable Linting for Cypress Code

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,24 @@
+const jestRules =  require('eslint-plugin-jest').rules;
+const jestDomRules = require('eslint-plugin-jest-dom').rules;
+const testLibRules = require('eslint-plugin-testing-library').rules;
+
+const disableAllRules = (pluginName, rules) => {
+	return Object.keys(rules).reduce((ac, ruleName) => {
+		return {
+			...ac,
+			[`${pluginName}/${ruleName}`] : 'off',
+		}
+	}, {});
+}
+
+// We need to turn off jest rules for cypress because
+// Cypress uses expect.
+const cypressRuleDisables = {
+	...(disableAllRules('jest', jestRules)),
+	...(disableAllRules('jest-dom', jestDomRules)),
+	...(disableAllRules('testing-library', testLibRules)),
+}
+
 module.exports = {
 	extends: '@nciocpl/eslint-config-react',
 	globals: {
@@ -5,4 +26,10 @@ module.exports = {
 		Cypress: true,
 		getFixture: true,
 	},
+	overrides: [
+		{
+			'files': ['cypress/**/*.js'],
+			'rules': cypressRuleDisables,
+		}
+	]
 };
