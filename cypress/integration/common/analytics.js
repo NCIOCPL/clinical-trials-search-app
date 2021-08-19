8af7c95ecfc433ce95fc2cd787568f9119011105
Bryan Pizzillo <bryanpizzillo@users.noreply.github.com>
2021-08-19 17:02:54 -0400

    (#276) Fix Cypress Linting Errors

--- cypress/integration/common/analytics.js
+++ cypress/integration/common/analytics.js
@@ -6,23 +6,26 @@ import { Then, When } from 'cypress-cucumber-preprocessor/steps';
  * @param {string} val the proposed value
  */
 const convertValue = (val) => {
-  if (typeof val === 'string') {
-    if (val.indexOf('(int)') === 0) {
-      return parseInt(val.replace('(int)', ''))
-    } else if (val.indexOf('(arr)') === 0) {
-      return val.replace('(arr)', '').split(',');
-    } else if (val.indexOf('(bool)') === 0) {
-      return val.replace('(bool)', '') === "true" ? true : false;
-    } else if (val.indexOf('(arrInt)') === 0) {
-      const convertedArr = [];
-      val.replace('(arrInt)', '').split(',').forEach(element => {
-        convertedArr.push(parseInt(element));
-      });
-      return convertedArr;
-    }
-  }
-  return val;
-}
+	if (typeof val === 'string') {
+		if (val.indexOf('(int)') === 0) {
+			return parseInt(val.replace('(int)', ''));
+		} else if (val.indexOf('(arr)') === 0) {
+			return val.replace('(arr)', '').split(',');
+		} else if (val.indexOf('(bool)') === 0) {
+			return val.replace('(bool)', '') === 'true' ? true : false;
+		} else if (val.indexOf('(arrInt)') === 0) {
+			const convertedArr = [];
+			val
+				.replace('(arrInt)', '')
+				.split(',')
+				.forEach((element) => {
+					convertedArr.push(parseInt(element));
+				});
+			return convertedArr;
+		}
+	}
+	return val;
+};
 
 /**
  * Converts an object with a.b.c styled keys into an
@@ -30,71 +33,61 @@ const convertValue = (val) => {
  * @param {object} obj The DataTable.hashes()
  */
 const convertAnalyticsDatatableObject = (obj) => {
-  const objPass1 = Object.keys(obj)
-    .map(key => {
-      const [first, ...rest] = key.split('.');
-      if (rest.length > 0) {
-        const newRestKey = rest.join('.');
-        const value = convertValue(obj[first + '.' + newRestKey]);
-        return {
-          first,
-          rest: {
-            [newRestKey]: value
-          }
-        }
-      } else {
-        return {
-          first,
-          value: obj[first]
-        }
-      }
-    })
-    .reduce(
-      (ac, curr) => {
-        const newKey = curr.first;
+	const objPass1 = Object.keys(obj)
+		.map((key) => {
+			const [first, ...rest] = key.split('.');
+			if (rest.length > 0) {
+				const newRestKey = rest.join('.');
+				const value = convertValue(obj[first + '.' + newRestKey]);
+				return {
+					first,
+					rest: {
+						[newRestKey]: value,
+					},
+				};
+			} else {
+				return {
+					first,
+					value: obj[first],
+				};
+			}
+		})
+		.reduce((ac, curr) => {
+			const newKey = curr.first;
 
-        if (curr.value !== undefined) {
-          if (ac[newKey]) {
-            throw new Error(`Key, ${newKey}, is already defined.`);
-          }
-          return {
-            [newKey]: curr.value,
-            ...ac
-          }
-        } else if (curr.rest) {
-          if (ac[newKey] && typeof ac[newKey] !== 'object') {
-            throw new Error(`Key, ${newKey}, is mixing values and objects.`);
-          }
+			if (curr.value !== undefined) {
+				if (ac[newKey]) {
+					throw new Error(`Key, ${newKey}, is already defined.`);
+				}
+				return {
+					[newKey]: curr.value,
+					...ac,
+				};
+			} else if (curr.rest) {
+				if (ac[newKey] && typeof ac[newKey] !== 'object') {
+					throw new Error(`Key, ${newKey}, is mixing values and objects.`);
+				}
 
-          return {
-            ...ac,
-            [newKey]: {
-              ...ac[newKey],
-              ...curr.rest
-            }
-          }
-
-        } else {
-          // This is not good
-          return ac;
-        }
-      },
-      {}
-    );
-  return Object.entries(objPass1)
-    .reduce(
-      (ac, [key, val] = {}) => {
-        return {
-          ...ac,
-          [key]: (typeof val === "object") ?
-            convertAnalyticsDatatableObject(val) :
-            val
-        }
-      },
-      {}
-    );
-
-}
+				return {
+					...ac,
+					[newKey]: {
+						...ac[newKey],
+						...curr.rest,
+					},
+				};
+			} else {
+				// This is not good
+				return ac;
+			}
+		}, {});
+	return Object.entries(objPass1).reduce((ac, [key, val] = {}) => {
+		return {
+			...ac,
+			[key]:
+				typeof val === 'object' ? convertAnalyticsDatatableObject(val) : val,
+		};
+	}, {});
+};
 
 /**
  * Finds an event with the following information.
@@ -102,16 +95,18 @@ const convertAnalyticsDatatableObject = (obj) => {
  * @param {string} event The name of the event
  */
 const getEventFromEDDL = (win, type, event) => {
-  return win.NCIDataLayer.filter(evt => evt.type === type && evt.event === event);
+	return win.NCIDataLayer.filter(
+		(evt) => evt.type === type && evt.event === event
+	);
 };
 
 When('the NCIDataLayer is cleared', () => {
-  cy.window().then((win) => {
-    // For now while win.NCIDataLayer is normal array we can reset the
-    // datalayer to an empty array.
-    win.NCIDataLayer = [];
-    console.log('win.NCIDataLayer has been reset');
-  });
+	cy.window().then((win) => {
+		// For now while win.NCIDataLayer is normal array we can reset the
+		// datalayer to an empty array.
+		win.NCIDataLayer = [];
+		console.log('win.NCIDataLayer has been reset');
+	});
 });
 
 // So this is going to find the page load event with the name, and then
@@ -119,113 +114,115 @@ When('the NCIDataLayer is cleared', () => {
 // matched event. Note we do not just look at the page here since in the
 // future we could have additional data points.
 Then(
-  'there should be an analytics event with the following details',
-  (datatable) => {
-    cy.window().then((win) => {
-      console.log('finding analytics event for analytics step');
-      // First convert the datatable into nested object.
-      const rawDataObj = convertAnalyticsDatatableObject(datatable.rowsHash());
-      // Gotta strip the header row. (key/value)
-      const dataObj = Object.entries(rawDataObj).reduce((ac, [key, val]) => {
-        if (key === 'key') {
-          return ac;
-        }
-        return {
-          ...ac,
-          [key]: val
-        }
-      }, {})
+	'there should be an analytics event with the following details',
+	(datatable) => {
+		cy.window().then((win) => {
+			console.log('finding analytics event for analytics step');
+			// First convert the datatable into nested object.
+			const rawDataObj = convertAnalyticsDatatableObject(datatable.rowsHash());
+			// Gotta strip the header row. (key/value)
+			const dataObj = Object.entries(rawDataObj).reduce((ac, [key, val]) => {
+				if (key === 'key') {
+					return ac;
+				}
+				return {
+					...ac,
+					[key]: val,
+				};
+			}, {});
 
-      if (!dataObj.event) {
-        throw new Error("Datatable is missing the event name")
-      }
+			if (!dataObj.event) {
+				throw new Error('Datatable is missing the event name');
+			}
 
-      if (!dataObj.type) {
-        throw new Error("Datatable is missing the event type")
-      }
+			if (!dataObj.type) {
+				throw new Error('Datatable is missing the event type');
+			}
 
-      // Find the matching events, this should be only one.
-      const matchedEvents = getEventFromEDDL(win, dataObj.type, dataObj.event);
-      expect(matchedEvents).to.have.length(1);
+			// Find the matching events, this should be only one.
+			const matchedEvents = getEventFromEDDL(win, dataObj.type, dataObj.event);
+			expect(matchedEvents).to.have.length(1);
 
-      const eventData = matchedEvents[0];
-      //this still need regex support, but new function checks for exact num of values in the event
-      expect(deepEqual(eventData, dataObj)).to.be.true
-    });
-  }
+			const eventData = matchedEvents[0];
+			//this still need regex support, but new function checks for exact num of values in the event
+			expect(deepEqual(eventData, dataObj)).to.be.true;
+		});
+	}
 );
 
 //this still need regex support, but new function checks for exact num of values in the event
 function deepEqual(eventObj, expectedObj) {
-  const keys1 = Object.keys(eventObj);
-  const keys2 = Object.keys(expectedObj);
+	const keys1 = Object.keys(eventObj);
+	const keys2 = Object.keys(expectedObj);
 
-  if (keys1.length !== keys2.length) {
-    return false;
-  }
+	if (keys1.length !== keys2.length) {
+		return false;
+	}
 
-  for (const key of keys1) {
-    const val1 = eventObj[key];
-    const val2 = expectedObj[key];
-    const areObjects = isObject(val1) && isObject(val2);
-    if (
-      areObjects && !deepEqual(val1, val2) ||
-      !areObjects && val1 !== val2
-    ) {
-      return false;
-    }
-  }
+	for (const key of keys1) {
+		const val1 = eventObj[key];
+		const val2 = expectedObj[key];
+		const areObjects = isObject(val1) && isObject(val2);
+		if (
+			(areObjects && !deepEqual(val1, val2)) ||
+			(!areObjects && val1 !== val2)
+		) {
+			return false;
+		}
+	}
 
-  return true;
+	return true;
 }
 
 function isObject(object) {
-  return object != null && typeof object === 'object';
+	return object != null && typeof object === 'object';
 }
 
 ///this is needed to capture event after eddl was refreshed and no longer contain past events (such as FormAbandon before page is reloaded.
 // It is using global cy.NCIDataLayer instead of win.NCIDataLayer and cy has to be set(copied from win.NCIDataLayer) in the navigation step that will trigger
-//page reload: 
+//page reload:
 //cy.window().then(win=>{
 //   cy.NCIDataLayer = Object.assign(win.NCIDataLayer)
 // })
 Then(
-  'there should be preserved analytics event with the following details',
-  (datatable) => {
-    cy.window().then((win) => {
-      console.log('finding analytics event for analytics step');
-      // First convert the datatable into nested object.
-      const rawDataObj = convertAnalyticsDatatableObject(datatable.rowsHash());
-      // Gotta strip the header row. (key/value)
-      const dataObj = Object.entries(rawDataObj).reduce((ac, [key, val]) => {
-        if (key === 'key') {
-          return ac;
-        }
-        return {
-          ...ac,
-          [key]: val
-        }
-      }, {})
+	'there should be preserved analytics event with the following details',
+	(datatable) => {
+		cy.window().then(() => {
+			console.log('finding analytics event for analytics step');
+			// First convert the datatable into nested object.
+			const rawDataObj = convertAnalyticsDatatableObject(datatable.rowsHash());
+			// Gotta strip the header row. (key/value)
+			const dataObj = Object.entries(rawDataObj).reduce((ac, [key, val]) => {
+				if (key === 'key') {
+					return ac;
+				}
+				return {
+					...ac,
+					[key]: val,
+				};
+			}, {});
 
-      if (!dataObj.event) {
-        throw new Error("Datatable is missing the event name")
-      }
+			if (!dataObj.event) {
+				throw new Error('Datatable is missing the event name');
+			}
 
-      if (!dataObj.type) {
-        throw new Error("Datatable is missing the event type")
-      }
+			if (!dataObj.type) {
+				throw new Error('Datatable is missing the event type');
+			}
 
-      // Find the matching events, this should be only one.
-      const matchedEvents = getPastEventFromEDDL(dataObj.type, dataObj.event);
-      expect(matchedEvents).to.have.length(1);
+			// Find the matching events, this should be only one.
+			const matchedEvents = getPastEventFromEDDL(dataObj.type, dataObj.event);
+			expect(matchedEvents).to.have.length(1);
 
-      const eventData = matchedEvents[0];
-      
-      expect(deepEqual(eventData, dataObj)).to.be.true
-    });
-  }
+			const eventData = matchedEvents[0];
+
+			expect(deepEqual(eventData, dataObj)).to.be.true;
+		});
+	}
 );
-///this is to use global cy instead of win 
+///this is to use global cy instead of win
 const getPastEventFromEDDL = (type, event) => {
-  return cy.NCIDataLayer.filter(evt => evt.type === type && evt.event === event);
+	return cy.NCIDataLayer.filter(
+		(evt) => evt.type === type && evt.event === event
+	);
 };
--- cypress/integration/common/index.js
+++ cypress/integration/common/index.js
@@ -2,180 +2,194 @@
 import { And, Given, Then, When } from 'cypress-cucumber-preprocessor/steps';
 import { fieldMap } from '../../utils/ctsFields.js';
 
-cy.on('uncaught:exception', (err, runnable) => {
-  // returning false here prevents Cypress from
-  // failing the test
-  return false;
+cy.on('uncaught:exception', () => {
+	// returning false here prevents Cypress from
+	// failing the test
+	return false;
 });
 
 Given('the user navigates to {string}', (destURL) => {
-  cy.visit(destURL);
+	cy.visit(destURL);
 });
 
 Given('{string} is set to {string}', (key, param) => {
-  cy.on('window:before:load', (win) => {
-    win.INT_TEST_APP_PARAMS[key] = param;
-  });
+	cy.on('window:before:load', (win) => {
+		win.INT_TEST_APP_PARAMS[key] = param;
+	});
 });
 
 When('the user navigates to {string}', (def) => {
-  cy.visit(def);
+	cy.visit(def);
 });
 
 Then('the page title is {string}', (title) => {
-  cy.get('h1').should('contain', title);
+	cy.get('h1').should('contain', title);
 });
 
 And('browser waits', () => {
-  cy.wait(2000);
+	cy.wait(2000);
 });
 
 Given('screen breakpoint is set to {string}', (screenSize) => {
-  if (screenSize === 'desktop') cy.viewport(1025, 600);
-  else if (screenSize === 'mobile') cy.viewport(600, 800);
-  else if (screenSize === 'tablet') cy.viewport(800, 900);
+	if (screenSize === 'desktop') cy.viewport(1025, 600);
+	else if (screenSize === 'mobile') cy.viewport(600, 800);
+	else if (screenSize === 'tablet') cy.viewport(800, 900);
 });
 
-
 When('user clicks on {string} field', (fieldLabel) => {
-  cy.get(`input#${fieldMap[fieldLabel]}`).click();
+	cy.get(`input#${fieldMap[fieldLabel]}`).click();
 });
 
 When('user types {string} in {string} field', (inputText, fieldLabel) => {
-  cy.get(`input#${fieldMap[fieldLabel]}`).type(inputText);
+	cy.get(`input#${fieldMap[fieldLabel]}`).type(inputText);
 });
 
 When('user scrolls down to {string}', (landmark) => {
-  cy.get('legend').contains(landmark).scrollIntoView();
-})
+	cy.get('legend').contains(landmark).scrollIntoView();
+});
 
 When('user clicks on {int} trial result', (resultItem) => {
-  cy.get('.results-list__item a').eq(resultItem - 1).click();
+	cy.get('.results-list__item a')
+		.eq(resultItem - 1)
+		.click();
 });
 
 And('user clicks on {string} button', (buttonLabel) => {
-  cy.get('button').contains(buttonLabel).click();
+	cy.get('button').contains(buttonLabel).click();
 });
 
 And('user clicks on {string} link', (linkText) => {
-  cy.get('a').contains(linkText).click({ followRedirect: false });
+	cy.get('a').contains(linkText).click({ followRedirect: false });
 });
 
 And('user navigates back to the previous page', () => {
-  cy.window().then(win => {
-    cy.NCIDataLayer = Object.assign(win.NCIDataLayer)
-  })
-  cy.go('back'), { followRedirect: false };
-})
+	cy.window().then((win) => {
+		cy.NCIDataLayer = Object.assign(win.NCIDataLayer);
+	});
+	cy.go('back'), { followRedirect: false };
+});
 And('user clears form', () => {
-  cy.window().then(win => {
-    cy.NCIDataLayer = Object.assign(win.NCIDataLayer)
-  })
-  cy.get('button').contains('Clear Form').click();
-})
+	cy.window().then((win) => {
+		cy.NCIDataLayer = Object.assign(win.NCIDataLayer);
+	});
+	cy.get('button').contains('Clear Form').click();
+});
 
 When('user scrolls to middle of screen', () => {
-  cy.scrollTo("center");
+	cy.scrollTo('center');
 });
 
 When('user checks {string} checkbox', (label) => {
-  cy.get('label').contains(label).parent().find('input').check({ force: true });
+	cy.get('label').contains(label).parent().find('input').check({ force: true });
 });
 
 When('user checks {string} checkbox on {int} pages', (label, page) => {
-  for (let i = 1; i <= page; i++) {
-    cy.get('div.pager__num').contains(`${i}`).click()
-    cy.get('label').contains(label).parent().find('input').check({ force: true });
-  }
-})
-
-When('user clicks on share by {string} button', (option) => {
-
-  if (option === 'Print') {
-    cy.window().then(win => {
-      //same approach we used on platform testig and it works, in the app it does not!(
-      //stubbing the print window to prevent the call to open it, 
-      //but still triggering the click event
-      const printStub = cy.stub(win, 'print')
-      cy.get("button").contains(option).click({ force: true });
-
-    })
-  } else {
-    cy.get('button[class="share-btn cts-share-email"]')
-      .then(button$ => {
-        button$.on('click', e => {
-          //this is useless since cypress still waits for a new page to load 
-          // and it never loads - timeout and fail!
-          e.preventDefault;
-        });
-      })
-      .click({ force: true });
-  }
+	for (let i = 1; i <= page; i++) {
+		cy.get('div.pager__num').contains(`${i}`).click();
+		cy.get('label')
+			.contains(label)
+			.parent()
+			.find('input')
+			.check({ force: true });
+	}
 });
 
+When('user clicks on share by {string} button', (option) => {
+	if (option === 'Print') {
+		cy.window().then((win) => {
+			//same approach we used on platform testig and it works, in the app it does not!(
+			//stubbing the print window to prevent the call to open it,
+			//but still triggering the click event
+			cy.stub(win, 'print');
+			cy.get('button').contains(option).click({ force: true });
+		});
+	} else {
+		cy.get('button[class="share-btn cts-share-email"]')
+			.then((button$) => {
+				button$.on('click', (e) => {
+					//this is useless since cypress still waits for a new page to load
+					// and it never loads - timeout and fail!
+					e.preventDefault;
+				});
+			})
+			.click({ force: true });
+	}
+});
 
 And('{string} form section is displayed', (sectionLabel) => {
-  cy.get('legend').contains(sectionLabel).should('be.visible');
+	cy.get('legend').contains(sectionLabel).should('be.visible');
 });
 
 And('button {string} is displayed', (buttonLabel) => {
-  cy.get('button').contains(buttonLabel).should('be.visible');
+	cy.get('button').contains(buttonLabel).should('be.visible');
 });
 
 Then('the search is executed and results page is displayed', () => {
-  cy.location('pathname').should('contain', 'search/r')
-  cy.get('h1').should('have.text', 'Clinical Trials Search Results');
-  cy.get('div[class="results-list"]').should('be.visible');
+	cy.location('pathname').should('contain', 'search/r');
+	cy.get('h1').should('have.text', 'Clinical Trials Search Results');
+	cy.get('div[class="results-list"]').should('be.visible');
 });
 
 Then('the search is executed and no results page is displayed', () => {
-  cy.location('pathname').should('contain', 'search/r')
-  cy.get('h1').should('have.text', 'Clinical Trials Search Results');
-  cy.get('div[class="results-list no-results"]').should('be.visible');
+	cy.location('pathname').should('contain', 'search/r');
+	cy.get('h1').should('have.text', 'Clinical Trials Search Results');
+	cy.get('div[class="results-list no-results"]').should('be.visible');
 });
 
 And('the url query has the following corresponding code', (dataTable) => {
-  cy.location('href').then(url => {
-    const params = new URL(url).searchParams;
-    //verify num of url params matches expected
-    expect(Array.from(params.entries()).length).to.eq(dataTable.rows().length);
-    //verify that url query params have expected values
-    const paramItem = Array.from(params.entries()).map(([pkey, pvalue]) => {
-      for (const { parameter, value } of dataTable.hashes()) {
-        if (parameter === pkey) {
-          expect(pvalue).equal(value);
-        }
-      }
-    });
-  })
+	cy.location('href').then((url) => {
+		const params = new URL(url).searchParams;
+		//verify num of url params matches expected
+		expect(Array.from(params.entries()).length).to.eq(dataTable.rows().length);
+		//verify that url query params have expected values
+		Array.from(params.entries()).map(([pkey, pvalue]) => {
+			for (const { parameter, value } of dataTable.hashes()) {
+				if (parameter === pkey) {
+					expect(pvalue).equal(value);
+				}
+			}
+		});
+	});
 });
 
 And('helper text {string} is displayed', (helperText) => {
-  cy.get('span').contains(helperText).should('be.visible');
+	cy.get('span').contains(helperText).should('be.visible');
 });
 
-Then('{string} input field has a placeholder {string}', (fieldLabel, placeholderText) => {
-  cy.get(`input#${fieldMap[fieldLabel]}`).should('have.attr', 'placeholder', placeholderText);
-});
+Then(
+	'{string} input field has a placeholder {string}',
+	(fieldLabel, placeholderText) => {
+		cy.get(`input#${fieldMap[fieldLabel]}`).should(
+			'have.attr',
+			'placeholder',
+			placeholderText
+		);
+	}
+);
 
-Then('autocomplete dropdown is displayed with {string} text', (autosuggestItem) => {
-  cy.get('div.cts-autocomplete__menu-item').should('have.text', autosuggestItem);
-});
-
+Then(
+	'autocomplete dropdown is displayed with {string} text',
+	(autosuggestItem) => {
+		cy.get('div.cts-autocomplete__menu-item').should(
+			'have.text',
+			autosuggestItem
+		);
+	}
+);
 
 And('{string} link has a href {string}', (linkText, linkHref) => {
-  cy.get('a').contains(linkText).should('have.attr', 'href', linkHref)
+	cy.get('a').contains(linkText).should('have.attr', 'href', linkHref);
 });
 
 And('the following delighters are displayed', (dataTable) => {
-  for (const { delighter, href, title, text } of dataTable.hashes()) {
-    cy.get(`div[class="delighter ${delighter}"]`).as('delighter');
-    cy.get('@delighter').find('a').should('have.attr', 'href', href);
-    cy.get('@delighter').find('h4').should('have.text', title);
-    cy.get('@delighter').find('p').should('have.text', text);
-  }
+	for (const { delighter, href, title, text } of dataTable.hashes()) {
+		cy.get(`div[class="delighter ${delighter}"]`).as('delighter');
+		cy.get('@delighter').find('a').should('have.attr', 'href', href);
+		cy.get('@delighter').find('h4').should('have.text', title);
+		cy.get('@delighter').find('p').should('have.text', text);
+	}
 });
 
 Then('help icon is displayed with href {string}', (helpHref) => {
-  cy.get("a.text-icon-help").should('have.attr', 'href', helpHref);
+	cy.get('a.text-icon-help').should('have.attr', 'href', helpHref);
 });
