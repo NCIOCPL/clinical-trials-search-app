010e3d668bd021a0a4f9ad8b7e980eb6bc94d42f
kate-mashkina <mashkinaea@nci-02181235-ml.myfiosgateway.com>
2021-08-27 17:05:41 -0400

    (#282) updated /disease mock

--- support/src/mock-clinical-trials/diseases.js
+++ support/src/mock-clinical-trials/diseases.js
@@ -30,7 +30,7 @@ const diseasesPost = async (req, res, next) => {
  * @param {Function} next
  */
 const diseasesGet = async (req, res, next) => {
-  const { code, current_trial_status, name, size, sort, type } = req.query;
+  const { code, current_trial_status, name, size, sort, type, ancestor_ids } = req.query;
 
   // First strip off current_trial_status and ensure it matches our required types.
   // This way we don't have to make it part of the file name.
@@ -41,21 +41,26 @@ const diseasesGet = async (req, res, next) => {
   }
 
   // File naming is
-  // <codes>_<types>_<size>_<sort>
+  // <codesOrName>_<types>_<size>_<sort> if request does not include ancestor ids (only primary cancer type)
+  //<codesOrName>_<types>_<size>_<sort>_<ancestor_id> when it does (when fetching subtype, stage, findings etc)
+  // if one or more params are missing, then name them 'empty': <codes>_empty_<size>_empty
   // codes can be 
   // C1-C2-C3 to separate the disease concepts
+  //if name has a space, replace it with - : <breast-cancer>
   // each disease concept can have multiple IDs comma separated
   // so we could have C1,C2-C3-C4,C5 if we have 3 concepts, C1,C2 and C3 and C4,C5
 
   // TODO: This needs to be sanitized.
-  const name_fragment = name ? name : "empty";
-
-  const code_fragment = code && Array.isArray(code) && code.length > 0 ? code.join('-') : "empty";
-  const type_fragment = type && Array.isArray(type) && type.length > 0 ? type.join('-') : "empty";
+  const name_fragment = name ? name.replaceAll(' ','-') : "empty";
+  const ancestor_fragment = ancestor_ids ? ancestor_ids : "";
+  const code_fragment = code && !Array.isArray(code) ? code : code && Array.isArray(code) && code.length > 0 ? code.join('-') : "empty";
+  const type_fragment = type && !Array.isArray(type) ? type : type && Array.isArray(type) && type.length > 0 ? type.join('-') : "empty";
   const size_fragment = size ? size : "empty";
   const sort_fragment = sort ? sort : "empty";
 
-  const fileName = `${code_fragment}_${type_fragment}_${size_fragment}_${sort_fragment}.json`;
+  const codesOrName = name ? name_fragment : code_fragment;
+  const partialFileName = `${codesOrName}_${type_fragment}_${size_fragment}_${sort_fragment}`;
+  const fileName = ancestor_ids ? `${partialFileName}_${ancestor_fragment}.json` : `${partialFileName}.json`;
 
   const mockFile = path.join(
     __dirname,
@@ -70,10 +75,10 @@ const diseasesGet = async (req, res, next) => {
     await accessAsync(mockFile);
     res.sendFile(mockFile);
   } catch (err) {
-      // Access denied to open file, or not found.
-      // treat at 404, or your choice.
-      console.error(err);
-      res.status(404).end();
+    // Access denied to open file, or not found.
+    // treat at 404, or your choice.
+    console.error(err);
+    res.status(404).end();
   }
 }
 
@@ -93,6 +98,6 @@ const mockDiseases = async (req, res, next) => {
     // Method not allowed
     res.status(405).end();
   }
-} 
+}
 
 module.exports = mockDiseases;
