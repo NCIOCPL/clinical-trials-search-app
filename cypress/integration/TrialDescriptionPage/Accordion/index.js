5f694993ed79d233c7f944826803cb8850c12978
kate-mashkina <mashkinaea@nci-02181235-ml.myfiosgateway.com>
2021-09-08 10:13:47 -0400

    (#279) Accordion location tests - added tests - added mocks

--- cypress/integration/TrialDescriptionPage/Accordion/index.js
+++ cypress/integration/TrialDescriptionPage/Accordion/index.js
@@ -65,3 +65,135 @@ And('{string} button as link is displayed', (buttonAsLink) => {
 And('text {string} is displayed', (text) => {
 	cy.get('p').contains(text).should('be.visible');
 });
+
+And('the matched location {string} is {string}', (category, name) => {
+	if (category.toLowerCase() === 'country') {
+		cy.get('.sites-nearby h3').should('have.text', name);
+	} else if (category.toLowerCase() === 'state') {
+		cy.get('.sites-nearby .location-state h4').should('have.text', name);
+	} else if (category.toLowerCase() === 'city') {
+		cy.get('.sites-nearby .location-city h5').should('have.text', name);
+	}
+});
+
+And('the matched location displays the following information', (dataTable) => {
+	cy.get('.sites-nearby .location').as('nearLocation');
+	cy.get('@nearLocation')
+		.find('strong')
+		.should('include.text', dataTable.raw()[0].toString());
+	for (let i = 0; i < dataTable.rows().length; i++) {
+		cy.get('@nearLocation')
+			.find('div')
+			.eq(i)
+			.should('have.text', dataTable.raw()[i + 1].toString());
+	}
+});
+
+When('user selects {string} from the state dropdown list', (state) => {
+	cy.get('select.cts-select').select(state);
+});
+
+And(
+	'the filtered by state location number {int} displays the following information',
+	(index, dataTable) => {
+		cy.get('.location-city')
+			.eq(index - 1)
+			.as('location');
+		cy.get('@location')
+			.find('h5')
+			.should('include.text', dataTable.raw()[0].toString());
+		cy.get('@location')
+			.find('strong')
+			.should('include.text', dataTable.raw()[1].toString());
+		for (let i = 0; i < dataTable.rows().length - 2; i++) {
+			cy.get('@location')
+				.find(' .location')
+				.find('div')
+				.eq(i)
+				.should('have.text', dataTable.raw()[i + 2].toString());
+		}
+	}
+);
+
+Then('the location section is empty', () => {
+	cy.get('.sites-all').should('be.empty');
+});
+
+Then('alphabetically sorted list of countries appears', () => {
+	let isSorted = true;
+	cy.document().then((document) => {
+		//get all the options from select
+		const options = document.querySelectorAll('select.cts-select'[0].children);
+		for (let i = 1; i < options.length - 1; i++) {
+			//localeCompare() returns a negative number when a reference string (option[i])
+			//comes before compareString (option[i+1])
+			if (
+				options[i].textContent.localeCompare(options[i + 1].textContent) > 0
+			) {
+				isSorted = false;
+				console.log(
+					`${options[i].textContent} is out of order with ${
+						options[i + 1].textContent
+					}`
+				);
+			}
+		}
+		expect(isSorted).to.be.true;
+	});
+});
+Then('alphabetically sorted list of states appears', () => {
+	let isSorted = true;
+	cy.document().then((document) => {
+		//get all the options from select
+		const options = document.querySelectorAll('select.cts-select'[1].children);
+		for (let i = 1; i < options.length - 1; i++) {
+			//localeCompare() returns a negative number when a reference string (option[i])
+			//comes before compareString (option[i+1])
+			if (
+				options[i].textContent.localeCompare(options[i + 1].textContent) > 0
+			) {
+				isSorted = false;
+				console.log(
+					`${options[i].textContent} is out of order with ${
+						options[i + 1].textContent
+					}`
+				);
+			}
+		}
+		expect(isSorted).to.be.true;
+	});
+});
+
+When('user selects {string} from countries list', (country) => {
+	cy.get('select.cts-select').eq(0).select(country);
+});
+
+And(
+	'the filtered by country location number {int} displays the following information',
+	(index, dataTable) => {
+		cy.get('.location-city:visible')
+			.eq(index - 1)
+			.as('location');
+		cy.get('@location')
+			.find('h5')
+			.should('include.text', dataTable.raw()[0].toString());
+		cy.get('@location')
+			.find('strong')
+			.should('include.text', dataTable.raw()[1].toString());
+		for (let i = 0; i < dataTable.rows().length - 2; i++) {
+			cy.get('@location')
+				.find(' .location')
+				.find('div')
+				.eq(i)
+				.should('have.text', dataTable.raw()[i + 2].toString());
+		}
+	}
+);
+
+And('button {string} is not displayed', (button) => {
+	cy.get('.btnAsLink').contains(button).should('not.exist');
+});
+
+And('all locations in {string} are displayed', (country) => {
+	cy.get('.sites-all h3').should('have.text', country);
+});
