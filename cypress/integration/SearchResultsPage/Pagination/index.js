9da70b360a8db8160463c987bcf212373b9284e9
seyilonge <seyilonge@gmail.com>
2021-09-09 09:53:52 -0400

    (#240) Search Results Page: Pagination
    Closes #240
    
    * Added feature file and steps command (WIP...)
    * Added scripts for test steps, added test data
    * Reverted formatting changes on package.json, moved navigation step script from common to Pagination

---
+++ cypress/integration/SearchResultsPage/Pagination/index.js
@@ -0,0 +1,70 @@
+import { And, Then, When } from 'cypress-cucumber-preprocessor/steps';
+
+/*
+	--------------
+		Navigation
+	--------------
+*/
+Then(
+	'the user is redirected to {string} with query parameters {string}',
+	(redirectUrl, queryParams) => {
+		cy.location('href').should('include', `${redirectUrl}?${queryParams}`);
+	}
+);
+
+/*
+	---------
+		Pager
+	---------
+*/
+Then('the system displays {string} {string}', (perPage, total) => {
+	cy.get('.paging-section__page-info')
+		.should('include.text', perPage)
+		.and('include.text', total);
+});
+
+And('result list is displayed', () => {
+	cy.get('div.results-list').should('be.visible');
+});
+
+And('pager displays the following navigation options', (dataTable) => {
+	const pagerItems = [];
+	for (const { pages } of dataTable.hashes()) {
+		pagerItems.push(pages);
+	}
+	let counter = 0;
+	// Verify that pager displays correct number of page items
+	cy.get('.pager__container:first .pager__nav div:visible').should(
+		'have.length',
+		pagerItems.length
+	);
+	cy.get('.pager__container:last .pager__nav div:visible').should(
+		'have.length',
+		pagerItems.length
+	);
+
+	// Verify that the order of displayed page items is correct
+	cy.get('.pager__container:first .pager__nav div:visible').each(($el) => {
+		cy.wrap($el).should('have.text', pagerItems[counter]);
+		counter++;
+	});
+});
+
+And('the page {string} is highlighted', (pageNum) => {
+	cy.get('.pager__container:first .pager__nav div.pager__num--active').should(
+		'have.text',
+		pageNum
+	);
+	cy.get('.pager__container:last .pager__nav div.pager__num--active').should(
+		'have.text',
+		pageNum
+	);
+});
+
+When('user clicks on {string} button', (arrow) => {
+	cy.get('.pager__container:first .pager__nav > div').contains(arrow).click();
+});
+
+When('pager is not displayed', () => {
+	cy.get('.pager__navigation li').should('not.exist');
+});
