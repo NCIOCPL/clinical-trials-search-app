66a2b6a3fa7fa307fbe0908ef81ec6953d224496
Adrian Cofie <acofie@sapient.com>
2022-02-02 10:39:29 -0500

    (#465)  Console Error  -- "Cannot update a component 'StateProvider'
    src/hooks/useAppInitializer.js
        * Updates app initialization logic to use a useEffect that runs once and only once
    
        Closes #465

--- src/hooks/useAppInitializer.js
+++ src/hooks/useAppInitializer.js
@@ -1,32 +1,20 @@
+import { useEffect } from 'react';
+
 // Context API store
 import { useAppSettings } from '../store/store';
 import { updateCTXGlobalValue } from '../store/ctx-actions';
-
-let internalAppHasBeenInitialized = false;
 
 export const useAppInitializer = () => {
 	// This must be called before any conditionals
 	const [, updateAppSettings] = useAppSettings();
 
-	// Prevent this hook from ever being called more than
-	// once. Kind of dirty I know...
-	if (internalAppHasBeenInitialized) {
-		return;
-	} else {
-		internalAppHasBeenInitialized = true;
-	}
-
-	const runQueryStuff = async () => {
+	// Dispatches the reducer action once and only once.
+	useEffect(() => {
 		updateAppSettings(
 			updateCTXGlobalValue({
 				field: 'appHasBeenInitialized',
 				value: true,
 			})
 		);
-	};
-
-	// Fire off the async function but do not wait for it to
-	// return. (Don't worry, react will stay running and at
-	// some point the appHasBeenInitialized will get updated)
-	runQueryStuff();
+	}, []);
 };
